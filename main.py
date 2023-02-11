6b6bc45b50db475d1c35a8d93ea26f03146e788b
Daniel Lahmanchuk <danlahmanchuk@gmail.com>
2023-02-11 13:10:38 -0800

    Change Print Statement Format

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 print("\nHello and welcome to my News Headline Aggregator.")
-print("It shows all of the major news headlines currently appearing in the United States.")
+print("It shows all of the major news headlines currently appearing in the United States.\n")
 
 response = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=e21aa0bd35ff43979b780a63369c86c3")
 data = response.json()
